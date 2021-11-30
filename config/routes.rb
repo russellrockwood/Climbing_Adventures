a7389830e23cabc4dda1ed69d632803fed6e27f1
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-30 11:00:02 -0500

    add new city route, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+
+  get '/cities', to: 'cities#index'
+  get '/cities/new', to: 'cities#new'
 end
