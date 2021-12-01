d969e67e21d4bfa60d8fa53772c6cfaf4b85ea48
Russell Rockwood <russell.rockwood@gmail.com>
2021-12-01 18:17:44 -0500

    setup simplecov/rspec

---
+++ spec/models/hostel_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe City do
+  # describe 'instance methods' do
+    # call an instance method on a class
+  # end
+
+
+  # describe 'class methods'
+  # describe 'relationships'
+  # describe 'validations'
+end
